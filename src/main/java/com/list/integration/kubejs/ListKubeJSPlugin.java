a1cea2d5c8a4e5d9c6272f974ebca3ffdd3d7d8c
DancingSnow <1121149616@qq.com>
2025-12-07 21:55:38 +0800

    修复报错

--- src/main/java/com/list/integration/kubejs/ListKubeJSPlugin.java
+++ src/main/java/com/list/integration/kubejs/ListKubeJSPlugin.java
@@ -2,9 +2,7 @@ package com.list.integration.kubejs;
 
 import com.list.all.ModRecipes;
 import com.list.integration.kubejs.recipe.FishPondRecipeSchema;
-import com.list.integration.kubejs.recipe.components.ItemStackComponent;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
-import dev.latvian.mods.kubejs.recipe.schema.RecipeComponentFactoryRegistryEvent;
 import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
 import dev.latvian.mods.kubejs.script.ScriptType;
 import dev.latvian.mods.kubejs.util.ClassFilter;
@@ -13,11 +11,6 @@ public class ListKubeJSPlugin extends KubeJSPlugin {
     @Override
     public void registerClasses(ScriptType type, ClassFilter filter) {
         filter.allow("com.list");
-    }
-
-    @Override
-    public void registerRecipeComponents(RecipeComponentFactoryRegistryEvent event) {
-        event.register("item_stack", ItemStackComponent.ITEM_STACK);
     }
 
     @Override
