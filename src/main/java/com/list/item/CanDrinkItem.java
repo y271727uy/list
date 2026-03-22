638db83c7ab750a1add36758be523437fd2886bb
y271727uy <145999682+y271727uy@users.noreply.github.com>
2026-03-22 20:41:39 +0800

    新模型

--- src/main/java/com/list/item/CanDrinkItem.java
+++ src/main/java/com/list/item/CanDrinkItem.java
@@ -3,55 +3,18 @@ package com.list.item;
 import com.list.all.ModItems;
 import javax.annotation.Nonnull;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.item.UseAnim;
 
-public class CanDrinkItem extends Item {
-    public CanDrinkItem(Properties properties) {
-        super(properties);
-    }
+public class CanDrinkItem extends DrinkItem {
+	public CanDrinkItem(Properties properties) {
+		super(properties);
+	}
 
-    @Override
-    public UseAnim getUseAnimation(ItemStack stack) {
-        return UseAnim.DRINK;
-    }
-
-    @Override
-    public SoundEvent getDrinkingSound() {
-        return SoundEvents.GENERIC_DRINK;
-    }
-
-    @Override
-    public SoundEvent getEatingSound() {
-        return getDrinkingSound();
-    }
-
-    @Override
-    public @Nonnull ItemStack finishUsingItem(@Nonnull ItemStack stack, @Nonnull Level level, @Nonnull LivingEntity livingEntity) {
-        ItemStack result = super.finishUsingItem(stack, level, livingEntity);
-        ItemStack can = new ItemStack(ModItems.CAN.get());
-
-        if (livingEntity instanceof Player player && player.getAbilities().instabuild) {
-            return result;
-        }
-
-        if (result.isEmpty()) {
-            return can;
-        }
-
-        if (livingEntity instanceof Player player) {
-            if (!player.getInventory().add(can)) {
-                player.drop(can, false);
-            }
-        } else {
-            livingEntity.spawnAtLocation(can);
-        }
-
-        return result;
-    }
+	@Override
+	public @Nonnull ItemStack finishUsingItem(@Nonnull ItemStack stack, @Nonnull Level level, @Nonnull LivingEntity livingEntity) {
+		ItemStack result = super.finishUsingItem(stack, level, livingEntity);
+		return result.isEmpty() ? new ItemStack(ModItems.CAN.get()) : result;
+	}
 }
+
--- src/main/java/com/list/item/DrinkItem.java
+++ src/main/java/com/list/item/DrinkItem.java
@@ -1,9 +1,14 @@
 package com.list.item;
 
+import javax.annotation.Nonnull;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.UseAnim;
 
 public class DrinkItem extends Item {
@@ -24,6 +29,30 @@ public class DrinkItem extends Item {
     @Override
     public SoundEvent getEatingSound() {
         return getDrinkingSound();
+    }
+
+    @Override
+    public @Nonnull ItemStack finishUsingItem(@Nonnull ItemStack stack, @Nonnull Level level, @Nonnull LivingEntity livingEntity) {
+        ItemStack result = super.finishUsingItem(stack, level, livingEntity);
+        ItemStack bottle = new ItemStack(Items.GLASS_BOTTLE);
+
+        if (livingEntity instanceof Player player && player.getAbilities().instabuild) {
+            return result;
+        }
+
+        if (result.isEmpty()) {
+            return bottle;
+        }
+
+        if (livingEntity instanceof Player player) {
+            if (!player.getInventory().add(bottle)) {
+                player.drop(bottle, false);
+            }
+        } else {
+            livingEntity.spawnAtLocation(bottle);
+        }
+
+        return result;
     }
 }
 
